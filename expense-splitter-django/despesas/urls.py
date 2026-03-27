8e92fef58c3a9552e326700c2b441d3863b7c50a
irlansales <irlansales00@gmail.com>
2026-03-27 14:58:10 -0300

    retomando o projeto

--- expense-splitter-django/despesas/urls.py
+++ expense-splitter-django/despesas/urls.py
@@ -6,15 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
 
 
-    path("biblioteca/", views.index, name='index'),
-
-
-    path("biblioteca/livros2", views.livros, name='livros'),
-
-    path("biblioteca/detalhe/<int:numero>/", views.detalhe_livro, name='detalhe'),
-
-
-    path("biblioteca/perfil/", views.perfil, name='perfil'),
-
-    path("biblioteca/autores/", views.autores, name='autores')
+  
 ]
