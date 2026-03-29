f08c4511bdbeff0f351335134bb27d45d5ae6587
irlansales <irlansales00@gmail.com>
2026-03-29 19:21:03 -0300

    recomeçando

--- expense-splitter-django/despesas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Usuario , Grupo, Gasto
-
-
-admin.site.register(Usuario)
-admin.site.register(Grupo)
-
-admin.site.register(Gasto)
--- models/pessoas.py
+++
@@ -1,7 +0,0 @@
-class Pessoa:
-    def __init__(self, nome, email,senha, sexo):
-        self.nome = nome
-        self.email = email
-        self.senha = senha
-        self.sexo = sexo
-        
